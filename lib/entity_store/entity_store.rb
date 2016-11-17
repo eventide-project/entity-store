df9df34f602981301b072111dbf3d08f81919853
Scott Bellware <sbellware@gmail.com>
2016-11-17 02:10:12 -0600

    Snapshot persistence class and interval are specified by the snapshot macro, rather than two macros

--- lib/entity_store/entity_store.rb
+++ lib/entity_store/entity_store.rb
@@ -120,7 +120,6 @@ module EntityStore
     project = projection_class.build(entity)
 
     logger.trace { "Reading (Stream Name: #{stream_name}, Position: #{current_position}" }
-    # EventSource::Postgres::Read.(stream_name, position: start_position, session: session) do |event_data|
     reader_class.(stream_name, position: start_position, session: session) do |event_data|
       project.(event_data)
       current_position = event_data.position
