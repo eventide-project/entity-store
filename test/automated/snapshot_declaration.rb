e9cb1a3b05b33f19465fbd4c6f542c79947abe49
Scott Bellware <sbellware@gmail.com>
2016-11-17 02:16:41 -0600

    Snapshot class and interval are assigned at once

--- test/automated/snapshot_declaration.rb
+++ test/automated/snapshot_declaration.rb
@@ -3,18 +3,27 @@ require_relative 'automated_init'
 context "Snapshot Declaration" do
   context "Snapshot is declared" do
     snapshot_class = Controls::Snapshot::Example
-    store = Controls::EntityStore.example(snapshot_class: snapshot_class)
+    snapshot_interval = '-1'
+    store = Controls::EntityStore.example(snapshot_class: snapshot_class, snapshot_interval: snapshot_interval)
 
     test "Persistent store is built and assigned to the store" do
-      assert store.cache.persistent_store.instance_of?(snapshot_class)
+      assert(store.cache.persistent_store.instance_of? snapshot_class)
+    end
+
+    test "Snapshot interval is assigned to the store" do
+      assert(store.snapshot_interval == snapshot_interval)
     end
   end
 
   context "Snapshot is not declared" do
-    store = Controls::EntityStore.example(snapshot_class: nil)
+    store = Controls::EntityStore.example(snapshot_class: nil, snapshot_interval: nil)
 
     test "Persistent store is not assigned to the store" do
-      assert store.cache.persistent_store.instance_of?(EntityCache::Storage::Persistent::None)
+      assert(store.cache.persistent_store.instance_of?(EntityCache::Storage::Persistent::None))
+    end
+
+    test "Snapshot interval is not assigned to the store" do
+      assert(store.snapshot_interval.nil?)
     end
   end
 end
