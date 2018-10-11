557eb53d08e53b94f53e0559d493ad4ac8b5e3f9
Nathan Ladd <nathanladd+github@gmail.com>
2018-10-11 13:48:29 -0500

    Read batch size, not reader batch size

--- test/automated/reader_declaration/optional_batch_size.rb
+++ test/automated/reader_declaration/optional_batch_size.rb
@@ -6,27 +6,27 @@ context "Reader Declaration" do
       batch_size = Controls::Reader::BatchSize.example
 
       context "Positional Argument" do
-        store = Controls::EntityStore.example(reader_batch_size: batch_size)
+        store = Controls::EntityStore.example(read_batch_size: batch_size)
 
         test "Given batch size is assigned to store" do
-          assert(store.reader_batch_size == batch_size)
+          assert(store.read_batch_size == batch_size)
         end
       end
 
       context "Keyword Argument" do
-        store = Controls::EntityStore.example(reader_batch_size_keyword: batch_size)
+        store = Controls::EntityStore.example(read_batch_size_keyword: batch_size)
 
         test "Given batch size is assigned to store" do
-          assert(store.reader_batch_size == batch_size)
+          assert(store.read_batch_size == batch_size)
         end
       end
     end
 
     context "Omitted" do
-      store = Controls::EntityStore.example(reader_batch_size: :none)
+      store = Controls::EntityStore.example(read_batch_size: :none)
 
-      test "Reader batch size is not set on store" do
-        assert(store.reader_batch_size.nil?)
+      test "Read batch size is not set on store" do
+        assert(store.read_batch_size.nil?)
       end
     end
   end
