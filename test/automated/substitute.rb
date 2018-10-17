ff334c29dc8b972a6d2e3921ea485efba716246a
Scott Bellware <sbellware@gmail.com>
2018-10-17 18:43:34 -0500

    Store substitute implements the delete interface

--- test/automated/substitute.rb
+++ test/automated/substitute.rb
@@ -65,4 +65,52 @@ context "Substitute" do
       end
     end
   end
+
+  context "Delete Cache Record" do
+    id = Controls::ID.example
+
+    context "Record Is in the Cache's Internal Store" do
+      record = Controls::Record.example
+
+      store = SubstAttr::Substitute.build(Controls::EntityStore.example_class)
+
+      internal_store = store.cache.internal_store
+
+      assert(internal_store.empty?)
+      internal_store.put(record)
+      assert(internal_store.count == 1)
+
+      record_count = internal_store.count
+
+      deleted_record = store.delete_cache_record(id)
+
+      test "Removes the cache record" do
+        assert(internal_store.count == record_count - 1)
+      end
+
+      test "Returns cache record" do
+        assert(deleted_record == record)
+      end
+    end
+
+    context "Record Is Not in the Cache's Internal Store" do
+      store = SubstAttr::Substitute.build(Controls::EntityStore.example_class)
+
+      internal_store = store.cache.internal_store
+
+      assert(internal_store.empty?)
+
+      record_count = internal_store.count
+
+      deleted_record = store.delete_cache_record(id)
+
+      test "Doesn't remove a cache record" do
+        assert(internal_store.count == record_count)
+      end
+
+      test "Returns nil" do
+        assert(deleted_record.nil?)
+      end
+    end
+  end
 end
