92b91c60027c3e90328431d9c71849f70e990781
Scott Bellware <sbellware@gmail.com>
2019-05-24 10:34:15 -0400

    Unbalanced parenthesis in a log output is corrected

--- lib/entity_store/entity_store.rb
+++ lib/entity_store/entity_store.rb
@@ -130,7 +130,7 @@ module EntityStore
 
     project = projection_class.build(entity)
 
-    logger.trace(tag: :refresh) { "Reading (Stream Name: #{stream_name}, Position: #{current_position}" }
+    logger.trace(tag: :refresh) { "Reading (Stream Name: #{stream_name}, Position: #{current_position})" }
     reader_class.(stream_name, position: start_position, batch_size: reader_batch_size, session: session) do |event_data|
       project.(event_data)
       current_position = event_data.position
